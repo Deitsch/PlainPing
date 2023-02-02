000f79b6758432745f34303ae4ddfb7dd1e45db9
Simon Deutsch <>
2023-02-02 17:16:15 +0100

    add SimplePing Source to PlainPing

--- Package.swift
+++ Package.swift
@@ -11,17 +11,16 @@ let package = Package(
             name: "PlainPing",
             targets: ["PlainPing"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/Deitsch/SimplePing", branch: "main")
-    ],
+    dependencies: [ ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PlainPing",
             dependencies: ["SimplePing"]),
+        .target(
+            name: "SimplePing",
+            dependencies: []),
         .testTarget(
             name: "PlainPingTests",
             dependencies: ["PlainPing"]),
